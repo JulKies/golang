7db1763ba545acc92b5965287830aa777338496c
JulKies <tester1234567@web.de>
2025-12-16 14:08:28 +0100

    - add process start - log file - gitignore

--- go-sro-bot/main.go
+++ go-sro-bot/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("Starting GO Bot")
+
+	//logger aufsetzten
+	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
+	log.Println("Starting GO Bot")
+
+	programPath := "C:\\Users\\kiese\\Desktop\\Sammelkiste\\golang\\sro\\silkroad.exe"
+
+	cmd := exec.Command(programPath)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	log.Println("Starte Programm:", programPath)
+
+	// Programm ausführen
+	if err := cmd.Run(); err != nil {
+		log.Println("Fehler beim Ausführen:", err)
+		os.Exit(1)
+	}
+
+	log.Println("Programm erfolgreich beendet")
 }
