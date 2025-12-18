6d254d4cdfaa9e7a77ee65cee62bc25f95c340d8
JulKies <tester1234567@web.de>
2025-12-18 23:09:15 +0100

    - remove bot logic

--- go-sro-bot/main.go
+++ go-sro-bot/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"os"
-	"os/exec"
 )
 
 func main() {
@@ -16,23 +15,4 @@ func main() {
 	defer f.Close()
 	log.SetOutput(f)
 
-	log.Println("Starting GO Bot")
-
-	programPath := "C:\\Users\\kiese\\Desktop\\Sammelkiste\\golang\\sro\\silkroad.exe"
-
-	cmd := exec.Command(programPath)
-
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	log.Println("Starte Programm:", programPath)
-
-	// Programm ausführen
-	if err := cmd.Start(); err != nil {
-		log.Println("Fehler beim Ausführen:", err)
-		os.Exit(1)
-	}
-
-	log.Printf("PID: %v", cmd.Process.Pid)
-	log.Println("Programm erfolgreich beendet")
 }
